6bfb51f26508b0feecd1931afc791821f9122f30
Bipin Lamichhane <superavitingeio@gmail.com>
2021-05-23 18:47:25 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+    $(document).ready(function(){
+        $("#show1").click(function(){
+            $("#workreq1").toggle();
+        });
+    })
+    
+    $(document).ready(function(){
+        $("#show2").click(function(){
+            $("#workreq2").toggle();
+        });
+    })
+    
+    $(document).ready(function(){
+        $("#show3").click(function(){
+            $("#workreq3").toggle();
+        });
+    })
+    $(document).ready(function(){
+        $("#show4").click(function(){
+            $("#workreq4").toggle();
+        });
+    });
+});
